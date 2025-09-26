667b12f7ad3a8369782aae076e1d737b20581170
afonsocrg <afonso.crg@gmail.com>
2025-09-27 01:16:27 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+<div align="center">
+  <img src="hackaboa_header.png" alt="Hackaboa - EST 2025" style="max-width: 400px;" />
+</div>
+
+A community-run group of indie makers / coders / designers / hardware hackers who build their own independent apps/startups, meeting up every Thursday for weekly co-working (and demos) in Lisboa, Portugal.
+
+Not in Lisboa? [More locations](https://hacka.network/?utm_source=hackaboa)
+
+If you code your own projects come and join us every Thursday from 9.15am at a co-working space in Lisboa.
+
+The usual schedule is:
+
+* 9.30am – Intros (a little about yourself + what you're working on)
+* 12:30pm – Local lunch together (if you want)
+* 4pm – Demos (share what you made today, or just watch)
+* 5pm – Drinks somewhere nearby 🍻🍷
+
+
+Please fill out [this form](https://hackaboa.com) and we'll send you the details!
---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import GitHubLogo from './assets/github.svg'
+
+export default function App() {
+  return (
+    <div className="w-full flex flex-col">
+      <a href="https://github.com/hacka-network/hackaboa" target="_blank" className="github-icon">
+        <img alt="Edit this on GitHub" width="30" height="30" src={GitHubLogo} />
+      </a>
+
+      <div className="p-8 mt-20 max-w-3xl mx-auto">
+        <h1 className="text-center text-avocado-500">Hackaboa 💻🤓🚃</h1>
+        <h2 className="text-center text-avocado-500 text-base mt-8 mb-16">
+          EST 2025. Part of{' '}
+          <a
+            href="https://hacka.network/?utm_source=hackaboa"
+            target="_blank"
+            className="underline text-[#3872EC] hover:text-[#3872EC]/60"
+          >
+            HACKA* NETWORK
+          </a>
+        </h2>
+        <p className="font-bold">
+          A community-run group of indie makers / coders / designers / hardware hackers who build their own independent apps/startups, meeting up every Thursday for weekly co-working (and demos) in Lisboa, Portugal.
+        </p>
+        <p className="text-base italic font-bold">
+          Not in Lisboa?{' '}
+          <a
+            href="https://hacka.network/?utm_source=hackaboa"
+            target="_blank"
+            className="underline text-[#3872EC] hover:text-[#3872EC]/60"
+          >
+            More locations
+          </a>
+        </p>
+
+        <div className="mt-8 text-left">
+          <p>
+            If you code <span className="font-bold">your own projects</span> come and join us every Thursday from 9.15am at a co-working space in Lisboa.
+          </p>
+
+          <p>The usual schedule is:</p>
+          <ul className="list-disc list-inside mb-4">
+            <li><span className="font-bold">9.30am – Intros</span> (a little about yourself + what you're working on)</li>
+            <li><span className="font-bold">12:30pm – Local lunch</span> together (if you want)</li>
+            <li><span className="font-bold">4pm – Demos</span> (share what you made today, or just watch)</li>
+            <li><span className="font-bold">5pm – Drinks</span> somewhere nearby 🍻🍷</li>
+          </ul>
+
+          <p className="font-semibold">Please fill out the form and we'll send you the details!</p>
+        </div>
+      </div>
+
+      <div className="flex flex-col h-[900px] overflow-hidden">
+        <iframe
+          className="flex-grow w-full -mt-[200px] h-[calc(100%+200px)]"
+          src="https://tally.so/r/mRpb79"
+          title="Hackaboa Registration Form"
+        />
+      </div>
+    </div>
+  )
+}
