fcbf28890745c0565124f9e5feb9c3b51430d0f4
afonsocrg <afonso.crg@gmail.com>
2025-12-06 15:56:48 +0000

    load tweets from public gist

--- src/TweetsSection.tsx
+++ src/TweetsSection.tsx
@@ -1,21 +1,34 @@
 import { Tweet } from 'react-tweet'
+import { useEffect, useState } from 'react'
+
+const GIST_URL = 'https://gist.githubusercontent.com/afonsocrg/039d0c8ff21ab254bb6e31f8a0e45070/raw/hackaboa_posts.json'
 
 export default function TweetsSection() {
+  const [tweets, setTweets] = useState<{ id: string }[]>([])
+  const [isLoading, setIsLoading] = useState(true)
 
-  // Sample tweets array
-  const tweets = [
-    { id: '1996595049400823973'},
-    { id: '1997006139230470345'},
-    { id: '1997258969442857262'},
-    { id: '1996900104872038612'},
-  ]
+  useEffect(() => {
+    fetch(GIST_URL)
+      .then(res => res.json())
+      .then(data => {
+        setTweets(data.map((id: string) => ({ id })))
+        setIsLoading(false)
+      })
+      .catch(err => {
+        console.error('Failed to load tweets:', err)
+        setIsLoading(false)
+      })
+  }, [])
 
   return (
     <div className="my-12">
       <p>Our latest meetups:</p>
 
-      <div className="overflow-x-auto scroll-smooth">
-        <div className="flex gap-4 pb-2">
+      {isLoading ? (
+        <p className="text-center py-8">Loading...</p>
+      ) : (
+        <div className="overflow-x-auto scroll-smooth">
+          <div className="flex gap-4 pb-2">
             {tweets.map(tweet => (
               <div
                 key={tweet.id}
@@ -26,6 +39,7 @@ export default function TweetsSection() {
             ))}
           </div>
         </div>
-      </div>
+      )}
+    </div>
   )
 }
